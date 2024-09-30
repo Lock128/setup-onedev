110fd6318948cfb38af7fc5849ef8d636c664b3a
Johannes Koch <johanneskoch@fico.com>
2024-09-30 06:54:45 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,39 @@
-# setup-onedev
+# Setting up Onedev on AWS ECS as a service
+
+This project sets up [Onedev](https://github.com/theonedev/onedev) on Amazon ECS Fargate running as a service using for CDK development with TypeScript.
+
+
+## What is Onedev?
+Onedev is an open source development platform packed with the power of code hosting and automated DevOps pipelines.
+
+You can use it as your self-hosted version of the now deprecated AWS CodeCommit.
+
+
+## Pre-Requisites
+- Set up an AWS account
+- Set up secrets for your repository/fork
+```
+        aws-access-key-id: ${{ secrets.AWS_ACCESS_KEY_ID }}
+        aws-secret-access-key: ${{ secrets.AWS_SECRET_ACCESS_KEY }}
+```
+
+## Deployment
+Once you push a change to your `main` branch a Github action will automatically deploy the CDK code.
+
+## What will be deployed?
+CDK will deploy [lib\setup-onedev-stack.ts](lib\setup-onedev-stack.ts) which includes:
+- an Amazon ECS Service that runs the latest official [Onedev container image](https://hub.docker.com/r/1dev/server)
+- an Amazon EFS that is used to store data
+
+Expected costs: [48 USD/month](https://calculator.aws/#/estimate?id=970c8026a305dd39246aa7d2665d5d398ad108d7) (when running in eu-central-1)
+
+## Useful commands
+
+The `cdk.json` file tells the CDK Toolkit how to execute your app.
+
+* `npm run build`   compile typescript to js
+* `npm run watch`   watch for changes and compile
+* `npm run test`    perform the jest unit tests
+* `npx cdk deploy`  deploy this stack to your default AWS account/region
+* `npx cdk diff`    compare deployed stack with current state
+* `npx cdk synth`   emits the synthesized CloudFormation template
---
+++ lib/setup-onedev-stack.ts
@@ -0,0 +1,146 @@
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
+import * as ecs from 'aws-cdk-lib/aws-ecs';
+import * as efs from 'aws-cdk-lib/aws-efs';
+import * as ecspatterns from 'aws-cdk-lib/aws-ecs-patterns';
+import * as logs from 'aws-cdk-lib/aws-logs';
+
+export class SetupOnedevStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    // Create a VPC with 9x subnets divided over 3 AZ's
+    const vpc = new ec2.Vpc(this, 'OnedevVPC', {
+      cidr: '172.34.0.0/16',
+      natGateways: 1,
+      maxAzs: 3,
+      subnetConfiguration: [
+        {
+          cidrMask: 20,
+          name: 'onedev-public',
+          subnetType: ec2.SubnetType.PUBLIC,
+        },
+        {
+          cidrMask: 20,
+          name: 'onedev-application',
+          subnetType: ec2.SubnetType.PRIVATE_WITH_EGRESS,
+        },
+        {
+          cidrMask: 20,
+          name: 'onedev-data',
+          subnetType: ec2.SubnetType.PRIVATE_ISOLATED,
+        },
+      ],
+    });
+
+    // Create an ECS cluster
+    const cluster = new ecs.Cluster(this, 'onedev-service-cluster', {
+      clusterName: 'onedev-service-cluster',
+      containerInsights: true,
+      vpc: vpc,
+    });
+
+
+    const webSecurityGroup = new ec2.SecurityGroup(this, "onedev-web-sg", {
+      securityGroupName: `Onedev-Web-app`,
+      vpc,
+    });
+
+    webSecurityGroup.addIngressRule(
+      webSecurityGroup,
+      ec2.Port.tcp(2049) // Enable NFS service within security group
+    );
+
+
+    const fileSystem = new efs.FileSystem(this, "EfsFileSystem", {
+      vpc,
+      securityGroup: webSecurityGroup
+    });
+
+    var accessPoint = new efs.AccessPoint(this, "volumeAccessPoint", {
+      fileSystem: fileSystem,
+      path: "/data",
+      createAcl: {
+        ownerGid: "0",
+        ownerUid: "0",
+        permissions: "755"
+      },
+      posixUser: {
+        uid: "0",
+        gid: "0"
+      }
+    });
+
+    const efsVolumeConfiguration: ecs.EfsVolumeConfiguration = {
+      authorizationConfig: {
+        accessPointId: accessPoint.accessPointId,
+        iam: 'ENABLED',
+      },
+      fileSystemId: fileSystem.fileSystemId,
+      transitEncryption: 'ENABLED',
+    };
+
+    //https://hub.docker.com/r/harness/harness
+
+    const taskDefinition = new ecs.TaskDefinition(this, 'onedev-web-task', {
+      compatibility: ecs.Compatibility.FARGATE,
+      memoryMiB: '512',
+      cpu: '256',
+
+    });
+    fileSystem.grantRootAccess(taskDefinition.taskRole);
+
+    const containerDefinition = {
+      image: ecs.ContainerImage.fromRegistry('1dev/server'),
+      logging: new ecs.AwsLogDriver({
+        streamPrefix: 'onedev',
+      }),
+      workingDirectory: '/data',
+      environment: {
+        'HARNESS_URL_BASE': 'http://setuph-harne-cmngr06fggq4-1705612358.eu-central-1.elb.amazonaws.com',
+        'GITNESS_URL_BASE': 'http://setuph-harne-cmngr06fggq4-1705612358.eu-central-1.elb.amazonaws.com'
+        
+      }
+    };
+/*
+    container.addMountPoints({
+      sourceVolume: assetVolume.name,
+      containerPath: "/mnt/assets",
+      readOnly: false,
+    });
+    */
+    
+    const volume = {
+      // Use an Elastic FileSystem
+      name: "data",
+      efsVolumeConfiguration
+    };
+    taskDefinition.addVolume(volume);
+    const container = taskDefinition.addContainer('defaultContainer', containerDefinition);
+    container.addPortMappings({
+      containerPort: 3000,
+      hostPort: 3000,
+      protocol: ecs.Protocol.TCP,
+    });
+    container.addMountPoints({
+      sourceVolume: volume.name,
+      containerPath: "/data",
+      readOnly: false,
+    });
+
+    // Create higher level construct containing the Fargate service with a load balancer
+    const service = new ecspatterns.ApplicationLoadBalancedFargateService(this, 'onedev-service', {
+      cluster,
+      circuitBreaker: {
+        rollback: true,
+      },
+      memoryLimitMiB: 512, // Supported configurations: https://docs.aws.amazon.com/cdk/api/v2/docs/aws-cdk-lib.aws_ecs_patterns.ApplicationMultipleTargetGroupsFargateService.html#memorylimitmib
+      cpu: 256,
+      desiredCount: 1,
+      taskDefinition: taskDefinition,
+      securityGroups: [webSecurityGroup]
+    });
+  }
+
+}
